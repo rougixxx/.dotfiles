e081f3d76788a2a09acf4ade249315af259c7e8a
rougixxx <m.remmane@esi-sba.dz>
2025-02-19 10:59:37 +0100

    fixed keyboard layout changing binds

--- configs/waybar/scripts/b.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Extract the current brightness using brightnessctl
-current_brightness=$(brightnessctl i | grep 'Current brightness' | awk '{print $4}' | tr -d '()' )
-current="${current_brightness}"
-echo $current
